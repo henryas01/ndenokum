8be5f8689e83db5b414e04c52acbbda6031d1599
henryAs <henry.albiri@renos.id>
2025-05-24 01:44:04 +0700

    fix

--- src/app/api/promo/route.ts
+++ src/app/api/promo/route.ts
@@ -1,17 +1,19 @@
-import { NextRequest, NextResponse } from 'next/server';
-import {connection} from '../utils/connection';
+import {  NextResponse } from 'next/server';
+import { connection } from '../utils/connection';
+
+// request?: NextRequest
 
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   // For example, fetch data from your DB here
-  let users = [];
+  let users:unknown[] = [];
 
 // A simple SELECT query
 try {
   const [results] = await connection.execute(
     'SELECT * FROM `hot_items`'
   );
-  users = results
+  users = results as unknown[];
 
   console.log(results); // results contains rows returned by server
 } catch (err) {
